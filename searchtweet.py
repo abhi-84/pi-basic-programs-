5f120272e128159414a88a49c8a10d6ce7380ae2
Abhishek kumar Srivastava <abhishek.embsys@gmail.com>
2019-03-26 15:36:26 +0530

    Update searchtweet.py

--- searchtweet.py
+++ searchtweet.py
@@ -1,9 +1,9 @@
 from twython import TwythonStreamer
 i=0
-C_KEY = "ATiON7RDMFjjVtQpBExfNpNrb"
-C_SECRET = "Tox1KBtpC8bQub984AcI0PxMDA1hgbtpZguMU3FpaXw8CT3YB5"
-A_TOKEN = "4914151872-1UOofoJ4DmgNrktYV3qr8NYZo7Z7mCqgpfvRdMi"
-A_SECRET = "VMAGKh66cER0qhwT14NP5XqxjkrXLiIwOKqh9FYffiXHc"
+C_KEY = "ATiON7RDMFjjVtQpxxxxpNrb"
+C_SECRET = "Tox1KBtpC8bQuxxxxxxxAcI0PxMDA1hgbtpZguMU3FpaXw8CT3YB5"
+A_TOKEN = "4914151872-1UOoxxxxxxxxgNrktYV3qr8NYZo7Z7mCqgpfvRdMi"
+A_SECRET = "VMAGKh66cER0qxxxxxxxxqxjkrXLiIwOKqh9FYffiXHc"
 
 class MyStreamer(TwythonStreamer):
     def on_success(self,data):
@@ -15,4 +15,4 @@ class MyStreamer(TwythonStreamer):
                 print("Ian G. Harris is popular")
 
 stream = MyStreamer(C_KEY, C_SECRET, A_TOKEN, A_SECRET)
-stream.statuses.filter(track= "Ian G. Harris")
+stream.statuses.filter(track= "Abhishek.centos")
